b75e20008d505748bf60d02cd036606d1227c4f7
matheusantonio <matheus.antonio.c@outlook.com>
2024-03-09 13:34:21 -0300

    feat: :bricks: Criado terraform para banco postgres no RDS

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,43 +1,14 @@
-data "aws_vpc" "new-vpc" {
+data "aws_vpc" "vpc" {
   filter {
     name   = "tag:Name"
     values = ["${var.prefix}-vpc"]
   }
 }
 
-data "aws_availability_zones" "available" {}
-
-resource "aws_subnet" "new-subnets" {
-    count = 2
-    availability_zone = data.aws_availability_zones.available.names[count.index]
-    vpc_id = data.aws_vpc.new-vpc.id
-    cidr_block = "10.0.${count.index+2}.0/24"
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "${var.prefix}-subnet-${count.index}"
-    }
+data "aws_subnets" "available-subnets" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
 }
 
-resource "aws_internet_gateway" "new-igw" {
-    vpc_id = data.aws_vpc.new-vpc.id
-    tags = {
-        Name = "${var.prefix}-igw"
-    }
-}
-
-resource "aws_route_table" "new-rtb" {
-    vpc_id = data.aws_vpc.new-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.new-igw.id
-    }
-    tags = {
-        Name = "${var.prefix}-rtb"
-    }
-}
-
-resource "aws_route_table_association" "new-rtb-association" {
-    count = 2
-    route_table_id = aws_route_table.new-rtb.id
-    subnet_id = aws_subnet.new-subnets.*.id[count.index]
-}
